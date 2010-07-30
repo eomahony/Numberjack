8f400b818681032b75e77fdb9e283366ba603024
ehebrard <emmanuel.hebrard@gmail.com>
2010-07-30 09:48:58 +0100

    archive

--- tools/mk_release.py
+++ tools/mk_release.py
@@ -8,21 +8,30 @@ version_number = '0'
 release_number = '1'
 svn_revision = ''
 
-subprocess.Popen(['svn', 'update'], stdout=subprocess.PIPE).wait()
-subprocess.Popen(['svn', 'commit', '-m', "archive"]).wait()
-update = subprocess.Popen(['svn', 'update'], stdout=subprocess.PIPE)
 
-update.wait()
+subprocess.Popen(['git', 'pull'], stdout=subprocess.PIPE).wait()
+subprocess.Popen(['git', 'commit', '-a', '-m', "archive"]).wait()
+#subprocess.Popen(['svn', 'update'], stdout=subprocess.PIPE).wait()
+#subprocess.Popen(['svn', 'commit', '-m', "archive"]).wait()
+#update = subprocess.Popen(['svn', 'update'], stdout=subprocess.PIPE)
+#update.wait()
+
+"""
 for line in update.stdout:
     print line,
     splitted = line.split()
     if len(splitted) == 3:
         if splitted[0] == 'At' and splitted[1] == 'revision':
             svn_revision = splitted[2][:-1]
+"""
+
+subprocess.Popen(['date', '"+%y-%m-%d"'], stdout=subprocess.PIPE).wait()
 
 suffix = version_number+'.'+release_number+'.'+svn_revision
 
-print version_number
+print suffix
+
+exit(1)
             
 subprocess.Popen( ['svn', 'export', 
                    'http://4c110.ucc.ie:8000/svn/Numberjack', 

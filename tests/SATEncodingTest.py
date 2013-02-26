47782b8b5fc11d5329f62561d995d3c2b2c50488
9thbit <9thbit@gmail.com>
2013-02-26 13:58:08 +0000

    Some unit tests targeted at SatWrapper.

---
+++ tests/SATEncodingTest.py
@@ -0,0 +1,316 @@
+'''
+
+This module is responsible for testing the SAT encodings of Numberjack.
+
+'''
+from Numberjack import *
+import unittest
+
+
+class SATEncodingTest(unittest.TestCase):
+    solver = None
+
+    # Tests checking that the overloaded Variable constructor conforms
+    # to its definition in the documentation.
+
+    def testEq1(self):
+        v1 = Variable(1, 5)
+        m = Model(v1 == 3)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value(), 3)
+
+    def testEq2(self):
+        v1 = Variable(1, 5)
+        v2 = Variable(3, 8)
+        m = Model(v1 == v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value(), v2.get_value())
+
+    def testEq3(self):
+        v1 = Variable(1, 4)
+        v2 = Variable(4, 6)
+        m = Model(v1 == v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value(), v2.get_value())
+
+    def testEqGap(self):
+        v1 = Variable([1, 3, 5, 7])
+        v2 = Variable(4, 8)
+        m = Model(v1 == v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value(), v2.get_value())
+
+    # def testConstantDomain(self):
+    #     v1 = Variable(1, 4)
+    #     v2 = Variable([2])
+    #     m = Model(v1 == v2)
+    #     s = SATEncodingTest.solver(m)
+    #     s.solve()
+    #     self.assertTrue(s.is_sat())
+    #     self.assertEqual(v1.get_value(), v2.get_value())
+
+    def testOffsetDomain(self):
+        v1 = Variable(1, 4)
+        v2 = Variable(1, 5)
+        m = Model(v1 + 3 == v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value() + 3, v2.get_value())
+
+    def testNe1(self):
+        v1 = Variable(1, 2)
+        m = Model(v1 != 1)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertNotEqual(v1.get_value(), 1)
+
+    def testNe2(self):
+        v1 = Variable(1, 2)
+        v2 = Variable(1, 2)
+        m = Model(v1 != v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertNotEqual(v1.get_value(), v2.get_value())
+
+    def testGt(self):
+        v1 = Variable(1, 3)
+        v2 = Variable(1, 3)
+        m = Model(v1 > v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertGreater(v1.get_value(), v2.get_value())
+
+    def testGe(self):
+        v1 = Variable(1, 3)
+        v2 = Variable(1, 3)
+        m = Model(v1 >= v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertGreaterEqual(v1.get_value(), v2.get_value())
+
+    def testLt(self):
+        v1 = Variable(1, 3)
+        v2 = Variable(1, 3)
+        m = Model(v1 < v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertLess(v1.get_value(), v2.get_value())
+
+    def testLe(self):
+        v1 = Variable(1, 3)
+        v2 = Variable(1, 3)
+        m = Model(v1 <= v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertLessEqual(v1.get_value(), v2.get_value())
+
+    def testLtGap(self):
+        v1 = Variable([1, 3, 5])
+        v2 = Variable([3])
+        m = Model(v1 < v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertLess(v1.get_value(), v2.get_value())
+
+    def testAddEqConstant(self):
+        v1 = Variable(5)
+        v2 = Variable(5)
+        m = Model(v1 + v2 == 6)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value() + v2.get_value(), 6)
+
+    def testAddEq(self):
+        v1 = Variable(5)
+        v2 = Variable(5)
+        v3 = Variable(5)
+        m = Model(v1 + v2 == v3)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value() + v2.get_value(), v3.get_value())
+
+    def testSubEq(self):
+        v1 = Variable(5)
+        v2 = Variable(5)
+        v3 = Variable(5)
+        m = Model(v1 - v2 == v3)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value() - v2.get_value(), v3.get_value())
+
+    def testAddMultiple(self):
+        v1 = Variable(5)
+        v2 = Variable(5)
+        v3 = Variable(5)
+        v4 = Variable(5)
+        v5 = Variable(5)
+        m = Model(v1 + v2 + v3 + v4 == v5)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value() + v2.get_value() + v3.get_value() + v4.get_value(), v5.get_value())
+
+    def testSumEq(self):
+        v1 = Variable(5)
+        v2 = Variable(5)
+        v3 = Variable(5)
+        m = Model(Sum([v1, v2]) == v3)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value() + v2.get_value(), v3.get_value())
+
+    def testSumArray(self):
+        vs = VarArray(4, 1, 5)
+        v_sum = Variable(5)
+        m = Model(Sum(vs) == v_sum)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(sum(v.get_value() for v in vs), v_sum.get_value())
+
+    def testMulConstant(self):
+        v1 = Variable(5)
+        v2 = Variable(5)
+        m = Model(v1 * 3 == v2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value() * 3, v2.get_value())
+
+    # FIXME Encoding multiplication between expressions is not supported yet.
+    # def testMulVars(self):
+    #     v1 = Variable(5)
+    #     v2 = Variable(5)
+    #     m = Model(v1 * v2 == 3)
+    #     s = SATEncodingTest.solver(m)
+    #     s.solve()
+    #     self.assertTrue(s.is_sat())
+    #     self.assertEqual(v1.get_value() * v2.get_value(), 3)
+
+    # def testMulVars(self):
+    #     v1 = Variable(5)
+    #     v2 = Variable(5)
+    #     v3 = Variable(5)
+    #     m = Model(v1 * v2 == v3)
+    #     s = SATEncodingTest.solver(m)
+    #     s.solve()
+    #     self.assertTrue(s.is_sat())
+    #     self.assertEqual(v1.get_value() * v2.get_value(), v3.get_value())
+
+    # def testDivConstant(self):
+    #     v1 = Variable(5)
+    #     v2 = Variable(5)
+    #     m = Model(v1 / 3 == v2)
+    #     s = SATEncodingTest.solver(m)
+    #     s.solve()
+    #     self.assertTrue(s.is_sat())
+    #     self.assertEqual(v1.get_value() / 3, v2.get_value())
+
+    def testAllDiff(self):
+        vs = VarArray(5, 1, 5)
+        m = Model(AllDiff(vs))
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        values = [v.get_value() for v in vs]
+        self.assertItemsEqual(range(1, 6), values)
+
+    def testMaximise(self):
+        v1 = Variable(5)
+        v2 = Variable(5)
+        m = Model(v1 < v2, Maximise(v1))
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value(), 3)
+        self.assertEqual(v2.get_value(), 4)
+
+    def testMinimise(self):
+        v1 = Variable(5)
+        v2 = Variable(5)
+        m = Model(v1 < v2, Minimise(v2))
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertEqual(v1.get_value(), 0)
+        self.assertEqual(v2.get_value(), 1)
+
+    def testFormula(self):
+        a, b, c = VarArray(3)
+        m = Model(((a == True) | (b == False)),
+                  ((b == True) | (a == False)),
+                  (c == True))
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertTrue(c.get_value())
+        self.assertEqual(a.get_value(), b.get_value())
+
+    def testFormula2(self):
+        a, b, c = VarArray(3)
+        m = Model(((a == True) | (b == False)) &
+                  ((b == True) | (a == False)) &
+                  (c == True))
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertTrue(c.get_value())
+        self.assertEqual(a.get_value(), b.get_value())
+
+    # FIXME Encoding of Table constraint is not supported yet.
+    # def testTable(self):
+    #     v1, v2 = VarArray(2, 1, 3)
+    #     t = Table([v1, v2], [[1, 1], [2, 2], [3, 3]])
+    #     m = Model(t)
+    #     s = SATEncodingTest.solver(m)
+    #     s.solve()
+    #     self.assertTrue(s.is_sat())
+    #     self.assertNotEqual(v1.get_value(), v2.get_value())
+
+    def testMax(self):
+        v1 = Variable(5)
+        v2 = Variable(5)
+        m = Model(Max([v1, v2]) < 2)
+        s = SATEncodingTest.solver(m)
+        s.solve()
+        self.assertTrue(s.is_sat())
+        self.assertLess(v1.get_value(), 2)
+        self.assertLess(v2.get_value(), 2)
+
+    # def testLargeDomain(self):
+    #     v1 = Variable(10000)
+    #     v2 = Variable(10000)
+    #     m = Model(v1 == v2)
+    #     s = SATEncodingTest.solver(m)
+    #     s.output_cnf("large_domain.cnf")
+    #     s.solve()
+    #     self.assertTrue(s.is_sat())
+    #     import sys
+    #     print >> sys.stderr, v1.get_value(), v2.get_value()
+
+    # def testAllDiffLargeDomain(self):
+    #     vs = VarArray(100, 1, 10000)
+    #     m = Model(AllDiff(vs))
+    #     s = SATEncodingTest.solver(m)
+    #     s.output_cnf("large_alldiff.cnf")
+    #     s.solve()
+    #     self.assertTrue(s.is_sat())
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,16 @@
 import Mistral
-from LinearTest import *
-from LogicalTest import *
-from GlobalsATest import *
-from MiscTest import *
-from CoreTest import *
+import MiniSat
+
+from LinearTest import LinearTest
+from LogicalTest import LogicalTest
+from GlobalsATest import GlobalsATest
+from MiscTest import MiscTest
+from CoreTest import CoreTest
+from SATEncodingTest import SATEncodingTest
 
 LinearTest.solver = Mistral.Solver
 LogicalTest.solver = Mistral.Solver
 MiscTest.solver = Mistral.Solver
 GlobalsATest.solver = Mistral.Solver
 CoreTest.solver = Mistral.Solver
+SATEncodingTest.solver = MiniSat.Solver

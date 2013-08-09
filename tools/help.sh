a522383ffb00b0bb6088f6fb89c84e2946eb7029
Simon de Givry <sdegivry@toulouse.inra.fr>
2013-08-09 16:27:18 +0200

    PATH should contain ../fzn

--- tools/help.sh
+++ tools/help.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 LOCAL_LIB=`pwd`/local_lib
+FZN_LIB=`pwd`/fzn
 
 echo  '  Remember to add the following lines to your .profile or .bashrc:'
 echo  '    PYTHONPATH='$LOCAL_LIB':$PYTHONPATH'
 echo  '    export PYTHONPATH'
 echo  '  and'
 echo  '    PATH='$LOCAL_LIB':$PATH'
+echo  '    PATH='$FZN_LIB':$PATH'
 echo  '    export PATH'
 echo  '  then run "source ~/.profile" or "source ~/.bashrc"'

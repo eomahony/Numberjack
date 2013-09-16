9da32bb316eee8a2a3c1460af91747d6de526620
9thbit <9thbit@gmail.com>
2013-09-16 12:01:04 +0200

    Simplify the output of help.sh for local_install so that the two lines can be copied easily.

--- tools/help.sh
+++ tools/help.sh
@@ -3,9 +3,6 @@
 LOCAL_LIB=`pwd`/local_lib
 
 echo  '  Remember to add the following lines to your .profile or .bashrc:'
-echo  '    PYTHONPATH='$LOCAL_LIB':$PYTHONPATH'
-echo  '    export PYTHONPATH'
-echo  '  and'
-echo  '    PATH='$LOCAL_LIB':$PATH'
-echo  '    export PATH'
+echo  '    export PYTHONPATH='$LOCAL_LIB':$PYTHONPATH'
+echo  '    export PATH='$LOCAL_LIB':$PATH'
 echo  '  then run "source ~/.profile" or "source ~/.bashrc"'

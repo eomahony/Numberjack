ff36287c4a30821b42ad8cd08455400c7abd9eb3
9thbit <9thbit@gmail.com>
2013-10-21 14:59:42 +0100

    Double quotes in XCSPOut.
    Some solvers had issues with single quotes.

--- src/XCSPOut.py
+++ src/XCSPOut.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#'''
+#"""
 #  Numberjack is a constraint satisfaction and optimisation library
 #  Copyright (C) 2009 Cork Constraint Computation Center, UCC
 #  
@@ -22,10 +22,10 @@
 import types
 
 class XCSPOutput():
-    '''
+    """
     This module handles outputting to XCSP 2.1 format
     Should be 2008 CSP Solver Competition rules compliant
-    '''
+    """
 
     # This maps the Numberjack global names to the XCSP ones
     global_map = {
@@ -45,7 +45,7 @@ class XCSPOutput():
         "nvalue"  : "nvalue",
         "nvalues" : "nvalues"
     }
-    '''
+    """
     Layout of the <parameters> section of global constraints
     S1     : Variable or Constant
     VARS   : List of variables and or constants
@@ -56,7 +56,7 @@ class XCSPOutput():
     ORTHOTOPES: Dunno
     ORTHOTOPE: Dunno
     TASKS: Dunno
-    '''
+    """
     global_format = {
         "AllDiff": ["VARS"],           "among": ["S1","VARS","VALS"],
         "AtLeast": ["S1","VARS","E1"], "AtMost": ["S1","VARS","E1"],
@@ -106,31 +106,31 @@ class XCSPOutput():
         self.njvar_mapping = {}
 
     def output(self, path):
-        '''
+        """
         Outputs the model to a given file.
-        '''
+        """
         self.load_variables()
         self.load_expressions()
 
         outfile = open(path, "w")
         outfile.write("<instance>\n" +
                       "<presentation " +
-                      "maxConstraintArity='%d' " % self.__maxConstraintArity +
-                      "format='XCSP 2.1' />\n")
+                      "maxConstraintArity=\"%d\" " % self.__maxConstraintArity +
+                      "format=\"XCSP 2.1\" />\n")
 
         self.output_domains(outfile)
         self.output_variables(outfile)
         self.output_relations(outfile)
         self.output_expressions(outfile)
 
-        outfile.write('\n</instance>\n')
+        outfile.write("\n</instance>\n")
         outfile.close()
 
     def load_variables(self):
-        '''
+        """
         Load in all the variables of the problem and create their
         domains and variable definitions in XML format
-        '''
+        """
         for var in self.__model.variables:
             domain = var.get_domain()
             self.njvar_mapping[var] = self.create_variable(domain[0],
@@ -138,9 +138,9 @@ class XCSPOutput():
                                                 var.ident, var.name())
 
     def load_expressions(self):
-        '''
+        """
         Loads all the expressions from the problem into XML format.
-        '''
+        """
         for con in self.__model.get_exprs():
             if self.global_map.get(con.get_operator()) != None:
                 self.handle_global_constraint(con)
@@ -153,24 +153,24 @@ class XCSPOutput():
                 self.__constraints.append(self.get_con(con))
 
     def get_pred(self, expr):
-        '''
+        """
         Handles the given predicate. Loads it in and stores it in XML format
-        '''
+        """
         self.__extract_param_counter = 0
         functional_expr = self.get_expr(expr)
 
         if self.pred_map.get(functional_expr) is None:
-            # We haven't seen this predicate before:
+            # We haven"t seen this predicate before:
             params = self.get_params(expr)
             count = 0
             for p in set(params):
                 count += 1
 
-            pred  = "\n\t<predicate name='P%d'>\n" % self.__local_pred_idx
+            pred = "\n\t<predicate name=\"P%d\">\n" % self.__local_pred_idx
             pred += "\t\t<parameters>\n\t\t\t"
             pred += "int X"+" int X".join([str(i) for i in range(count)])
             pred += "\n\t\t</parameters>\n"
-            
+
             pred += "\t\t<expression>\n"
             pred += "\t\t\t<functional>\n"
             pred += "\t\t\t\t%s\n" % functional_expr
@@ -183,10 +183,10 @@ class XCSPOutput():
             return pred
 
     def get_con(self, expr):
-        '''
+        """
         This creates the constraint nescessary for the given expression.
         It adds it to the list of constraints.
-        '''
+        """
         params = []
         self.extract_params_for_constraint(expr, params)
         parameters, dvars = [], []
@@ -206,7 +206,7 @@ class XCSPOutput():
         pred_ident  = self.pred_map.get(pred_string)[1]
 
         con  = "\n\t<constraint "
-        con += "name='C%d' arity='%d' scope='%s' reference='P%d'>\n" % (
+        con += "name=\"C%d\" arity=\"%d\" scope=\"%s\" reference=\"P%d\">\n" % (
                     self.__local_con_idx,
                     arity,
                     " ".join([str(i) for i in dvars]),
@@ -220,26 +220,26 @@ class XCSPOutput():
         return con
 
     def get_params(self, expr):
-        '''
+        """
         Returns the list of normalised parameters for the given expression
-        '''
+        """
         params = []
         self.__extract_param_counter = 0
         self.extract_params(expr, params)
         return params
 
     def get_expr(self, expr):
-        '''
+        """
         Returns the expression in string form, normalised parameters
-        '''
+        """
         self.__dVars = {}
         self.__extract_param_counter = 0
         return self.extract_expr(expr)
 
     def extract_params(self, expr, params):
-        '''
+        """
         Extracts the normalised parameters from the given expression
-        '''
+        """
         if type(expr) == types.IntType:
             params.append(("INT", self.__extract_param_counter))
             self.__extract_param_counter += 1
@@ -261,10 +261,10 @@ class XCSPOutput():
                 self.extract_params(ex, params)
 
     def extract_expr(self, expr):
-        '''
+        """
         Extracts a string representation of the given expression
         with normalised parameteres.
-        '''
+        """
         extracted_expr = None
         if type(expr) == types.IntType:
             self.__extract_param_counter += 1
@@ -310,10 +310,10 @@ class XCSPOutput():
         return extracted_expr
 
     def extract_params_for_constraint(self, expr, params):
-        '''
+        """
         Extracts the parameters in terms of their variable identifiers and
         wheather they are constants.
-        '''
+        """
         if type(expr) == types.IntType:
             params.append(("INT", expr))
         elif type(expr) == types.StringType:
@@ -329,9 +329,9 @@ class XCSPOutput():
                 self.extract_params_for_constraint(ex, params)
 
     def handle_global_constraint(self, expr):
-        '''
+        """
         This adds a global constraint to the list of constraints
-        '''
+        """
         if self.global_map.get(expr.get_operator()) is "NOT_SUPPORTED":
             print "Global operator, %s, is not supported, yet" % expr.get_operator()
         else:
@@ -339,13 +339,14 @@ class XCSPOutput():
             params = []
             for exp in expr.get_children():
                 self.extract_params_for_constraint(exp, params)
-            
+
             # Create intermediate variable for storing result of Element
             index = None
             if expr.get_operator() is "Element":
                 index = self.create_variable(1, len(expr.get_children())-1,
                                              Name="Result of Element C%d" %
                                                 self.__local_con_idx)
+                print "C%d created aux element var" % (self.__local_con_idx), index
                 params.append(("VAR", index))
             dparams = sorted(set(params))
             scope = [str(par[1]) for par in dparams if par[0] == "VAR"]
@@ -354,8 +355,8 @@ class XCSPOutput():
             if arity > self.__maxConstraintArity:
                 self.__maxConstraintArity = arity
 
-            con_string = "\n\t<constraint name='C%d' " % self.__local_con_idx
-            con_string += "arity='%d' scope='V%s' reference='global:%s'>\n" % (
+            con_string = "\n\t<constraint name=\"C%d\" " % self.__local_con_idx
+            con_string += "arity=\"%d\" scope=\"V%s\" reference=\"global:%s\">\n" % (
                 arity,
                 " V".join(scope),
                 self.global_map.get(expr.get_operator())
@@ -376,14 +377,13 @@ class XCSPOutput():
 
             if "S1" in conformat:
                 val = expr.get_children()[0]
-                if type(val) == types.IntType :
+                if isinstance(val, types.IntType):
                     con_string += "\t\t\t%d\n" % val
                 else:
                     con_string += "\t\t\tV%d\n" % val.ident
-                    
+
             if "VARS" in conformat:
                 if expr.get_operator() is "Element":
-                    print "ELEMENT"
                     con_string += "V%d\n\t\t\t" % index
                     del(params[-1])
                 con_string += "["
@@ -426,7 +426,7 @@ class XCSPOutput():
                 if type(val) == types.IntType :
                     con_string += "\t\t\t%d\n" % val
                 else:
-                    con_string += "\t\t\tV%d\n" % val.ident
+                    con_string += "\t\t\tVV%d\n" % val.ident
 
             con_string += "\t\t</parameters>\n\t</constraint>\n"
             self.__constraints.append(con_string)
@@ -434,10 +434,10 @@ class XCSPOutput():
             return index
 
     def handle_weighted_sum(self, expr):
-        '''
+        """
         This handles a weighted sum. It posts the sum constraint and links
         back to a predicate tree with an intermediate variable that is created.
-        '''
+        """
         # Get the children
         children = [self.handle_extra_expression(child)
                     for child in expr.get_children()]
@@ -474,9 +474,9 @@ class XCSPOutput():
         if arity > self.__maxConstraintArity:
             self.__maxConstraintArity = arity
 
-        con_string = "\n\t<constraint name='C%d' " % self.__local_con_idx
-        con_string += "arity='%d' scope='%s' " % ( arity, scope )
-        con_string += "reference='global:weightedSum'>\n"
+        con_string = "\n\t<constraint name=\"C%d\" " % self.__local_con_idx
+        con_string += "arity=\"%d\" scope=\"%s\" " % ( arity, scope )
+        con_string += "reference=\"global:weightedSum\">\n"
         self.__local_con_idx += 1
 
         con_string += "\t\t<parameters>\n\t\t\t"
@@ -495,10 +495,10 @@ class XCSPOutput():
         return idx
 
     def handle_extra_expression(self, expr):
-        '''
+        """
         Handles an expression that needs to be represented by an
         intermediate variable. Mainly for stuff under a weighted sum.
-        '''
+        """
         if type(expr) == types.IntType:
             return expr
         elif expr.is_var():
@@ -539,7 +539,7 @@ class XCSPOutput():
             if self.pred_map.get(function) is None:
                 pred_idx = self.__local_pred_idx
                 self.__local_pred_idx += 1
-                pred_string = "\n\t<predicate name='P%d'>\n" % pred_idx
+                pred_string = "\n\t<predicate name=\"P%d\">\n" % pred_idx
                 pred_string += "\n\t\t<parameters>\n"
                 pred_string += "\t\t\t\t%s\n" % params
                 pred_string += "\t\t</parameters>\n"
@@ -558,8 +558,8 @@ class XCSPOutput():
             else:
                 pred_idx = self.pred_map.get(function)[1]
 
-            con_string = "\n\t<constraint name='C%d'" % self.__local_con_idx
-            con_string += " arity='%d' scope='%s' reference='P%d'>\n" % (
+            con_string = "\n\t<constraint name=\"C%d\"" % self.__local_con_idx
+            con_string += " arity=\"%d\" scope=\"%s\" reference=\"P%d\">\n" % (
                 arity,
                 scope,
                 pred_idx
@@ -575,10 +575,10 @@ class XCSPOutput():
             return (var_idx, lb, ub)
 
     def handle_relations(self, expr):
-        '''
+        """
         Handles conflict and support relations
         adds relation and constraint to XCSP document
-        '''
+        """
         tuples = expr.parameters[0]
         semantics = expr.parameters[1]
         scope = expr.get_children()
@@ -588,15 +588,15 @@ class XCSPOutput():
         elif semantics is "support":
             semantics = "supports"
 
-        relation  = "\t<relation name='R%d'" % self.__local_rel_idx
-        relation += " arity='%d'" % len(scope)
-        relation += " nbTuples='%d'" % len(tuples)
-        relation += " semantics='%s'>\n\t\t" % semantics
+        relation  = "\t<relation name=\"R%d\"" % self.__local_rel_idx
+        relation += " arity=\"%d\"" % len(scope)
+        relation += " nbTuples=\"%d\"" % len(tuples)
+        relation += " semantics=\"%s\">\n\t\t" % semantics
         relation += "|".join([" ".join([str(t) for t in tuple]) for tuple in tuples])
         relation += "\n\t</relation>\n"
         self.__relations.append(relation)
         
-        constraint  = "\n\t<constraint name='C%d' arity='%d' scope='%s' reference='R%d' />\n" % (
+        constraint  = "\n\t<constraint name=\"C%d\" arity=\"%d\" scope=\"%s\" reference=\"R%d\" />\n" % (
             self.__local_con_idx,
             len(scope),
             " ".join(["V%d" % var.ident for var in scope]),
@@ -611,58 +611,58 @@ class XCSPOutput():
         self.__local_con_idx += 1
 
     def output_domains(self, outfile):
-        '''
+        """
         Outputs all the domains to the file
-        '''
-        outfile.write("\n<domains nbDomains='%d'>" % (
+        """
+        outfile.write("\n<domains nbDomains=\"%d\">" % (
             len(self.__domains)))
         for dom in self.__domains:
             outfile.write(dom)
         outfile.write("</domains>\n")
 
     def output_variables(self, outfile):
-        '''
+        """
         Outputs all the created variables to the file
-        '''
-        outfile.write("\n<variables nbVariables='%d'>\n" % (
+        """
+        outfile.write("\n<variables nbVariables=\"%d\">\n" % (
             len(self.__variables)))
         for var in self.__variables:
             outfile.write(var)
         outfile.write("</variables>\n")
         
     def output_relations(self, outfile):
-        '''
+        """
         Outputs all the relations to the file
-        '''
+        """
         if len(self.__relations)>0:
-            outfile.write("\n<relations nbRelations='%d'>\n" % (
+            outfile.write("\n<relations nbRelations=\"%d\">\n" % (
                     len(self.__relations)))
             for rel in self.__relations:
                 outfile.write(rel)
             outfile.write("</relations>\n")
 
     def output_expressions(self, outfile):
-        '''
+        """
         Print all the constraints and predicates to the file
-        '''
+        """
         if len(self.__predicates)>0:
-            outfile.write("\n<predicates nbPredicates='%d'>" % len(self.__predicates))
+            outfile.write("\n<predicates nbPredicates=\"%d\">" % len(self.__predicates))
             for pred in self.__predicates:
                 outfile.write(pred)
             outfile.write("</predicates>\n")
 
-        outfile.write("\n<constraints nbConstraints='%d'>" % len(self.__constraints))
+        outfile.write("\n<constraints nbConstraints=\"%d\">" % len(self.__constraints))
         for con in self.__constraints:
             outfile.write(con)
         outfile.write("</constraints>\n")
 
     def create_variable(self, lb, ub, domain=None, index=None, Name=None):
-        '''
+        """
         Creates a variable in the variable list for the XML. If the domain
         has already been specified it uses the previously specified domain.
         Otherwise it also creates a domain, automatically reducing sequencial
         numbers in the domain to intervals
-        '''
+        """
         
         if domain is None:
             domain = [lb,ub]
@@ -699,7 +699,7 @@ class XCSPOutput():
                 else:
                     dom_list.append("%d" % interval[0])
 
-        dom_string += "\n\t<domain name='D%d' nbValues='%d'>\n\t\t" % (
+        dom_string += "\n\t<domain name=\"D%d\" nbValues=\"%d\">\n\t\t" % (
                 d_idx, nbvalues)
 
         dom_list_string = " ".join(dom_list)
@@ -713,8 +713,8 @@ class XCSPOutput():
         else:
             d_idx = self.dom_map.get(dom_list_string)
 
-        var_string = "\t<variable name='V%d' domain='D%d'/>" % (idx, d_idx)
-        if Name != None and Name != "x":
+        var_string = "\t<variable name=\"V%d\" domain=\"D%d\"/>" % (idx, d_idx)
+        if Name is not None and Name != "x":
             var_string += "\t<!-- %s -->" % Name
         var_string += "\n"
 
@@ -722,9 +722,9 @@ class XCSPOutput():
         return idx
 
 def change_to_intervals(domain):
-    '''
+    """
     Represents the given domain as a list of intervals
-    '''
+    """
     intervals = []
     interval_start = domain[0]
     for i in range(0, len(domain) - 1):

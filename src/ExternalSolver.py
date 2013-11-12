f480f1a208871f79ebe7fa48710835751ed7b90d
9thbit <9thbit@gmail.com>
2013-11-12 18:15:52 +0000

    ExternalCNFSolver calls self.parse_solver_info_line for lines beginning with 'd ' or 'c '.

--- src/ExternalSolver.py
+++ src/ExternalSolver.py
@@ -258,8 +258,8 @@ class ExternalCNFSolver(ExternalSolver, SatWrapperSolver):
                         values.add(v)
                 # print "time to add this value line: %.4f" % (datetime.datetime.now() - t).total_seconds()
 
-        #     elif first_two == "d " or first_two == "c ":
-        #         self.parse_solver_info_line(line[2:])
+            elif first_two == "d " or first_two == "c ":
+                self.parse_solver_info_line(line[2:])
 
         t = datetime.datetime.now()
         self.store_solution(values)
@@ -276,7 +276,6 @@ class ExternalXCSPIntVariable(object):
         self.value = None
 
     def get_value(self):
-        print "ExternalXCSPIntVariable.get_value", str(self.value)
         return self.value
 
 
@@ -319,44 +318,10 @@ class ExternalXCSPSolver(ExternalSolver):
                 elif "SATISFIABLE" in line or "SAT" in line:
                     self.sat = Numberjack.SAT
             elif first_two == "v ":
-                print "Valued:", line
                 values.extend(map(int, line[2:].split()))
             elif first_two == "d " or first_two == "c ":
                 self.parse_solver_info_line(line[2:])
 
         if self.sat == Numberjack.SAT:
             for i, variable in enumerate(self.variables):
-                print "Setting", variable, "equal to:", values[i]
                 variable.value = values[i]
-
-    def parse_solver_info_line(self, line):
-        bits = [a.strip() for a in line.split() if len(a.strip()) > 0]  # Strip additional whitespace
-        for i in xrange(0, len(bits), 2):
-            try:
-                name, value = bits[i], bits[i + 1]
-                print name, value
-                if name == "NODES" or name == "NDS":
-                    self.nodes = int(value)
-                elif name == "BACKTRACKS":
-                    self.backtracks = int(value)
-                elif name == "CHECKS":
-                    self.checks = int(value)
-                elif name == "FAILURES":
-                    self.failures = int(value)
-            except Exception as e:
-                print >> sys.stderr, str(e)
-                pass
-
-
-# class Solver(Numberjack.NBJ_STD_Solver):
-#     def __init__(self, model=None, X=None, FD=False, clause_limit=-1, encoding=None):
-#         # if X:
-#         #     import sys
-#         #     print >> sys.stderr, "Warning explicitly specifying the decision variables for an external CSP solver is currently not supported."
-
-#         # We pass an empty model to NBJ_STD_Solver to prevent it from loading
-#         # and trying to decompse each expression.
-#         Numberjack.NBJ_STD_Solver.__init__(self, "ExternalXCSP", "ExternalXCSP", None, None, FD, clause_limit, encoding)
-#         self.solver_id = model.getSolverId()
-#         self.model = model
-#         self.solver.set_model(model, self.solver_id, self.Library, solver=self)

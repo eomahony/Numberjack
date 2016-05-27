cceabd6c15e35792ea6a143a203c674d5d6de43c
9thbit <9thbit@gmail.com>
2016-05-27 08:46:51 +0100

    Add missing semi-colon in Mistral2 interface.

--- Numberjack/solvers/Mistral2/Mistral2.cpp
+++ Numberjack/solvers/Mistral2/Mistral2.cpp
@@ -1872,7 +1872,7 @@ void Mistral2Solver::setRandomSeed(const int seed)
 #endif
 
   solver->parameters.seed = seed;
-	solver->initialise_random_seed(seed)
+	solver->initialise_random_seed(seed);
 
 }
 
